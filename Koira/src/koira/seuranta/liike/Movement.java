4d6f33f5b41cbd771fc59c7eac6bdbc1b904c4e8
Anton <Anton@Arawan-Len>
2017-02-17 13:27:49 +0200

    Liike

--- Koira/src/koira/seuranta/liike/Movement.java
+++ Koira/src/koira/seuranta/liike/Movement.java
@@ -1,5 +1,42 @@
 package koira.seuranta.liike;
 
-public class Movement {
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
 
+public class Movement {
+	private EV3LargeRegulatedMotor rmotor;
+	private EV3LargeRegulatedMotor lmotor;
+	
+	public Movement() {
+		this.rmotor = new EV3LargeRegulatedMotor(MotorPort.A);
+		this.lmotor = new EV3LargeRegulatedMotor(MotorPort.D);
+		rmotor.synchronizeWith(new RegulatedMotor[] {this.lmotor});
+	}
+	
+	void forward() {
+		this.rmotor.startSynchronization();
+		this.rmotor.backward();
+		this.lmotor.backward();
+		this.rmotor.endSynchronization();
+	}
+	
+	void back() {
+		this.rmotor.startSynchronization();
+		this.rmotor.forward();
+		this.lmotor.forward();
+		this.rmotor.endSynchronization();
+	}
+
+	void turn() {
+		this.rmotor.startSynchronization();
+		this.rmotor.endSynchronization();
+	}
+
+	void stop() {
+		this.rmotor.startSynchronization();
+		this.rmotor.stop();
+		this.lmotor.stop();
+		this.rmotor.endSynchronization();
+	}
 }
